6ff4384545504fe3df51b03d34abd497792fe0a6
Sona <s.harutyunyan1992@gmail.com>
2023-10-09 12:31:42 +0400

    JUnit4

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+plugins {
+    id("java")
+}
+
+group = "ru.netology"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    //testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    //testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation ("junit:junit:4.13")
+}
+
+
+
+tasks.test {
+    //useJUnitPlatform()
+    useJUnit()
+}
---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,38 @@
+package ru.netology.service;
+
+import org.junit.Test;
+
+
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+    @Test
+    public void shouldTestWhenItsUnder1000() {
+        CashbackHackService service = new CashbackHackService();
+
+        int expected = 100;
+        int actual = service.remain(900);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void shouldTestWhenItsAbove1000() {
+        CashbackHackService service= new CashbackHackService();
+
+        int expected=800;
+        int actual= service.remain(1200);
+        assertEquals(expected,actual);
+
+    }
+    @Test
+    public void shouldTestWhenIts1000(){
+        CashbackHackService service= new CashbackHackService();
+
+        int expected=0;
+        int actual= service.remain(1000);
+        assertEquals(expected,actual);
+    }
+
+}
